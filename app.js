b78f38da2b105b6a3cd01400983cf8f915a29fd7
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-01-18 16:42:33 +0100

    Add security packages

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 require('express-async-errors');
+// librairies de sécurité supplémentaires
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 const express = require('express');
 const app = express();
@@ -14,7 +19,19 @@ const todosRouter = require('./routes/todos.js');
 const notFound = require('./middleware/not-found.js');
 const errorHandler = require('./middleware/error-handler.js');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //* routes
 app.use('/api/v1/auth', authRouter);
